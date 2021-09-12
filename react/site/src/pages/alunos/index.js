7a2a25bc1b1ee872c6b5b29a6ed01f58c08dc37c
rafaelladamiski <rafaelladamiski@gmail.com>
2021-09-12 13:33:31 -0300

    ok

--- react/site/src/pages/alunos/index.js
+++ react/site/src/pages/alunos/index.js
@@ -1,13 +1,120 @@
-
 import Cabecalho from '../../components/cabecalho'
+
 import Menu from '../../components/menu'
 
 import { Container, Conteudo } from './styled'
 
+import { useState, useEffect, React, useRef } from 'react'
+
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import LoadingBar from 'react-top-loading-bar'
+
+import Api from '../service/api.js'
+const api = new Api();
+
 
 export default function Index() {
+    const [alunos, setAlunos] = useState([]);
+    const [nome, setNome] = useState('');
+    const [chamada, setChamada] = useState('');
+    const [turma, setTurma] = useState('');
+    const [curso, setCurso] = useState('');
+    const [idAlterando, setIdAlterando] = useState(0)
+    const loading = useRef();
+
+    async function listar () {
+
+        loading.current.continuousStart();
+        let r = await api.listar();
+        setAlunos(r);
+        loading.current.complete();
+
+    }
+
+    useEffect(() => { 
+        listar();
+    },
+     [])
+
+    async function inserir () {
+        loading.current.continuousStart();
+        if(chamada > 0){
+        if(idAlterando === 0){
+            let r = await api.inserir(nome, chamada, curso, turma);
+            if(r.erro)
+                toast.dark(r.erro)
+            else
+                toast.dark('Aluno inserido!')}
+
+        } else {
+            let r = await api.alterar(idAlterando, nome, chamada, curso, turma);
+            if (r.erro)
+               toast.dark(r.erro)
+            else
+            toast.dark('Aluno alterado!')
+        } if (chamada < 0) (
+            toast.dark('Chamada Inválida')
+        )
+
+        limparCampos();
+        listar();
+        loading.current.complete()
+    }
+
+    function limparCampos () {
+        setNome('')
+        setChamada('')
+        setCurso('')
+        setTurma('')
+        setIdAlterando(0);
+    }
+    async function remover (id) {
+        loading.current.continuousStart();
+
+        confirmAlert({
+            title: 'Remover aluno',
+            message: `Tem certeza que deseja remover o aluno ${id}?`,
+            buttons: [
+                {
+                    label: 'Sim, remover',
+                    onClick: async() => {
+                        let r = await api.remover(id)
+                        if(r.erro) {
+                            toast.dark(r.erro)
+                        } else {
+                            toast.dark('Aluno removido!')
+                        }
+                        listar();
+                    }
+                },
+                {
+                    label: 'Não, Cancelar'}
+            ]
+        })
+        listar();
+
+        loading.current.complete()
+    }
+
+    async function editar (item) {
+        setNome(item.nm_aluno);
+        setChamada(item.nr_chamada);
+        setCurso(item.nm_curso);
+        setTurma(item.nm_turma);
+        setIdAlterando(item.id_matricula);
+    }
+
+
     return (
+
         <Container>
+            <ToastContainer/>
+            <LoadingBar color="#EA10C7" ref={loading} />
             <Menu />
             <Conteudo>
                 <Cabecalho />
@@ -16,32 +123,32 @@ export default function Index() {
                         
                         <div class="text-new-student">
                             <div class="bar-new-student"></div>
-                            <div class="text-new-student">Novo Aluno</div>
+                            <div class="text-new-student"> {idAlterando === 0 ? "Novo Aluno" : "Alterando aluno " + idAlterando}</div>
                         </div>
 
                         <div class="input-new-student"> 
                             <div class="input-left">
                                 <div class="agp-input"> 
                                     <div class="name-student"> Nome: </div>  
-                                    <div class="input"> <input /> </div>  
+                                    <div class="input"> <input type="text" value={nome} onChange={e => setNome(e.target.value)}/> </div>  
                                 </div> 
                                 <div class="agp-input">
                                     <div class="number-student"> Chamada: </div>  
-                                    <div class="input"> <input /> </div> 
+                                    <div class="input"> <input type="text" value={chamada} onChange={e => setChamada(e.target.value)}/> </div> 
                                 </div>
                             </div>
 
                             <div class="input-right">
                                 <div class="agp-input">
                                     <div class="corse-student"> Curso: </div>  
-                                    <div class="input"> <input /> </div>  
+                                    <div class="input"> <input type="text" value={curso} onChange={e => setCurso(e.target.value)} /> </div>  
                                 </div>
                                 <div class="agp-input">
                                     <div class="class-student"> Turma: </div>  
-                                    <div class="input"> <input /> </div> 
+                                    <div class="input"> <input type="text" value={turma} onChange={e => setTurma(e.target.value)}/> </div> 
                                 </div>
                             </div>
-                            <div class="button-create"> <button> Cadastrar </button> </div>
+                            <div class="button-create"> <button onClick={inserir}> {idAlterando === 0 ? "Cadastrar": "Alterar"} </button> </div>
                         </div>
                     </div>
 
@@ -52,59 +159,24 @@ export default function Index() {
                         </div>
                     
                         <table class ="table-user">
-                            <thead>
-                                <tr>
-                                    <th> ID </th>
-                                    <th> Nome </th>
-                                    <th> Chamada </th>
-                                    <th> Turma </th>
-                                    <th> Curso </th>
-                                    <th class="coluna-acao"> </th>
-                                    <th class="coluna-acao"> </th>
-                                </tr>
-                            </thead>
+                
                     
                             <tbody>
-                                <tr>
-                                    <td> 1 </td>
-                                    <td> Fulao da Silva Sauro</td>
-                                    <td> 15 </td>
-                                    <td> InfoX </td>
-                                    <td> Informática </td>
-                                    <td> <button> <img src="/assets/images/edit.svg" alt="" /> </button> </td>
-                                    <td> <button> <img src="/assets/images/trash.svg" alt="" /> </button> </td>
-                                </tr>
-                            
-                                <tr class="linha-alternada">
-                                    <td> 1 </td>
-                                    <td> Fulao da Silva Sauro</td>
-                                    <td> 16 </td>
-                                    <td> InfoX </td>
-                                    <td> Informática </td>
-                                    <td> </td>
-                                    <td> </td>
-                                </tr>
 
-                                <tr>
-                                    <td> 1 </td>
-                                    <td> Fulao da Silva Sauro</td>
-                                    <td> 17 </td>
-                                    <td> InfoX </td>
-                                    <td> Informática </td>
-                                    <td> </td>
-                                    <td> </td>
-                                </tr>
 
-                                <tr class="linha-alternada">
-                                    <td> 1 </td>
-                                    <td> Fulao da Silva Sauro</td>
-                                    <td> 18 </td>
-                                    <td> InfoX </td>
-                                    <td> Informática </td>
-                                    <td> </td>
-                                    <td> </td>
-                                </tr>
+                            {alunos.map((item, i) =>
                                 
+                                <tr className={i % 2 === 0 ? "linha-alternada": ""}>
+                                    <td> {item.id_matricula} </td>
+                                    <td title={item.nm_aluno}> {item.nm_aluno != null && item.nm_aluno.length >= 25 ? item.nm_aluno.substr(0, 25) + '...' : item.nm_aluno}</td>
+                                    <td> {item.nr_chamada} </td>
+                                    <td> {item.nm_turma} </td>
+                                    <td> {item.nm_curso} </td>
+                                    <td className="coluna-acao"> <button onClick={ () => editar(item)}> <img src="/assets/images/edit.svg" alt="" /> </button> </td>
+                                    <td className="coluna-acao"> <button onClick={ () => remover(item.id_matricula) }> <img src="/assets/images/trash.svg" alt="" /> </button> </td>
+                                </tr>
+
+                            )}
                             </tbody> 
                         </table>
                     </div>
--- react/site/src/pages/alunos/styled.js
+++ react/site/src/pages/alunos/styled.js
@@ -1,44 +1,36 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-
     display: flex;
     flex-direction: row;
     min-width: 100%;
-
     .new-student-box {
         display: flex;
         flex-direction: column;
-
         background-color: white;
         box-shadow: 0px 0px 4px 1px rgba(186, 186, 186, 0.25);
         padding: 2em;
             
     }
-
     .bar-new-student {
         border: 3px solid #986CDF;
         border-radius: .5em;
         margin-right: .4em;
         background-color: #986CDF;
     }
-
     .text-new-student {
         display: flex;
         flex-direction: row;
-
         font-size: 32px;
     }
     .text-new-student div {
         font-weight: bolder;
     }
-
     .input-new-student {
         display: flex;
         flex-direction: row;
         padding-top: 2.4em;
     }
-
     .agp-input {
         display: flex;
         flex-direction: row;
@@ -48,31 +40,25 @@ const Container = styled.div`
         margin-top: .5em;
         margin-bottom: .5em;
     }
-
     .input-left {
         display: flex;
         flex-direction: column;
         margin-right: 3em;
     }
-
     .input-right {
         display: flex;
         flex-direction: column;
         margin-bottom: 1em;
     }
-
     input {
         width: 209px;
         height: 36px;
-
         background: #FFFFFF;
         border: 1px solid #A8A8A8;
         box-sizing: border-box;
         border-radius: 5px;
         padding-left: .5em;
-
     }
-
     .name-student {
         font-size: 18px;
         color: #615858;
@@ -80,40 +66,32 @@ const Container = styled.div`
         padding-left: 1em;
         font-weight: 800;
     }
-
-
     .number-student {
         font-size: 18px;
         color: #615858;
         width: 5.5em;
         font-weight: 800;
     }
-
     .class-student {
         font-size: 18px;
         color: #615858;
         margin-right: 0.8em;
         font-weight: 800;
     }
-
     .corse-student {
         font-size: 18px;
         color: #615858;
         margin-right: 1em;
         font-weight: 800;
     }
-
-
     .button-create {
         align-self: flex-end;
         justify-self: flex-end;
         padding-bottom: 1.5em;
     }
-
     .button-create  button {
         width: 106px;
         height: 36px;
-
         background: #E911C6;
         border-radius: 44px;
         border: none;
@@ -122,11 +100,9 @@ const Container = styled.div`
         margin-left: 2em;
         font-weight: bold;
     }
-
     .student-registered-box {
         display: flex;
         flex-direction: column;
-
         background-color: white;
         box-shadow: 0px 0px 4px 1px rgba(186, 186, 186, 0.25);
         padding: 2em;
@@ -140,12 +116,10 @@ const Container = styled.div`
         font-size: 32px;
         padding-left: .3em;
     }
-
     .row-bar {
         display: flex;
         flex-direction: row;
     }
-
     .information-purple {
         display: flex;
         flex-direction: row;
@@ -159,9 +133,6 @@ const Container = styled.div`
         font-size: 18px ;
         margin-bottom: 1em;
     }
-
-
-
     .gray{
         width: 1454px;
         height: 61.93px;
@@ -172,7 +143,6 @@ const Container = styled.div`
         align-items: center;
         font-size: 18px;
     }
-
     .white {
         width: 1454px;
         height: 61.93px;
@@ -183,31 +153,24 @@ const Container = styled.div`
         align-items: center;
         font-size: 18px;
     }
-
     .box-information {
         border: 1px solid #E2E2E2;
     }
-
     .body-right-box {
         display: flex;
         flex-direction: column;
-
         background-color: #F5F5F5;
-
         height: 100%;
         padding: 3em;
         
         margin-bottom: 0em;
         margin-right: 0em;
     }
-
-
     .reader-right-box {
         margin-top: 0em;
         padding-right: 0em;
         
     }
-
     td button {
         border-radius: 50%;
         background-color: #565656;
@@ -219,20 +182,15 @@ const Container = styled.div`
     .box-image {
         margin-right: 2em;
     }
-
     thead {
         background-color: #986CDF;
     }
-
     table {
         margin-top: 2em;
     }
-
-
     tbody {
         background-color: #F5F5F5;
     }
-
     td {
         text-align: left;
         height:  61.93px;
@@ -240,7 +198,6 @@ const Container = styled.div`
         color: #6D6868;
         font-weight: 600;
     }
-
     th {
         height: 61.93px;
         text-align: left;
@@ -252,28 +209,31 @@ const Container = styled.div`
     .table-user {
         border-collapse: collapse;
     }
-
     .linha-alternada {
         background-color: #fff;
     }
-
     .coluna-acao {
         width: .1em;
     }
-
+    .coluna-acao > button {
+        visibility: hidden;
+    }
+    tr:hover {
+        .coluna-acao > button {
+        visibility: visible;
+        }
+        
+    }
     button {
         cursor: pointer;
     }
-
     .button-create button:hover {
         background-color: #aa3997;
         transition: 2s;
     }
-
     img {
         cursor: pointer;
     }
-
     .absolute {
         color: white;
         background-color: #DB21BD;
@@ -285,7 +245,6 @@ const Container = styled.div`
         text-align: center;
         font-size: .7em;
     }
-
     .user-image {
         display: flex;
         justify-content: flex-end;
@@ -293,7 +252,6 @@ const Container = styled.div`
         border-radius: 50%;
         
     }
-
     .user-image img {
         width: 57px;
         height: 57px;
